1cf5f41342f73750412e808a87bce43487f060a2
jwoodmansee1 <jwoodmansee@wilsonelectronics.com>
2019-04-23 15:51:47 -0600

    updating the api key

--- main.go
+++ main.go
@@ -1,17 +1,51 @@
 package main
 
 import (
+	"errors"
 	"log"
 	"net/http"
 	"os"
+	"products-api/auth"
 	"products-api/controller"
 
+	jwtmiddleware "github.com/auth0/go-jwt-middleware"
 	_ "github.com/denisenkom/go-mssqldb"
+	jwt "github.com/dgrijalva/jwt-go"
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
 
 func main() {
+
+	// Set up jwt middleware
+	jwtMiddleware := jwtmiddleware.New(jwtmiddleware.Options{
+		ValidationKeyGetter: func(token *jwt.Token) (interface{}, error) {
+
+			// Verify 'aud' claim
+			audience := os.Getenv("AUDIENCE")
+			checkAudience := token.Claims.(jwt.MapClaims).VerifyAudience(audience, false)
+			if !checkAudience {
+				return token, errors.New("invalid audience")
+			}
+
+			// Verify 'iss' claim
+			iss := os.Getenv("DOMAIN")
+			checkIss := token.Claims.(jwt.MapClaims).VerifyIssuer(iss, false)
+			if !checkIss {
+				return token, errors.New("invalid issuer")
+			}
+
+			cert, err := auth.GetPemCert(token)
+			if err != nil {
+				panic(err)
+			}
+
+			result, _ := jwt.ParseRSAPublicKeyFromPEM([]byte(cert))
+			return result, nil
+		},
+		SigningMethod: jwt.SigningMethodRS256,
+	})
+
 	r := mux.NewRouter()
 
 	r.HandleFunc("/tags", controller.GetTags)
