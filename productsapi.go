803829cbc7eb772407407318815658fe6057160f
Kevin Bateman <kbateman@wilsonelectronics.com>
2019-05-03 15:52:11 -0600

    removed main function and package

--- productsapi.go
+++ productsapi.go
@@ -1,13 +1,8 @@
-package main
+package productsapi
 
 import (
-	"fmt"
-	"log"
-	"net/http"
-	"os"
 	"products-api/controller"
 
-	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
 
@@ -23,33 +18,4 @@ func NewRouter() *mux.Router {
 
 	r.HandleFunc("/product/{handle}", controller.GetProduct)
 	return r
-}
-
-func main() {
-	r := NewRouter()
-
-	methods := handlers.AllowedMethods([]string{"GET"})
-	headers := handlers.AllowedHeaders([]string{"Content-Type", "*"})
-	origins := handlers.AllowedOrigins([]string{"https://localhost:3000",
-		"http://localhost:3000",
-		"http://localhost:3001",
-		"https://localhost:4000",
-		"http://localhost:4000",
-		"https://wilsonpro.ca",
-		"https://wilsonpro.com",
-		"https://www.weboost.com/",
-		"https://staging.weboost.com/",
-		"https://staging.wilsonpro.ca",
-		"https://staging.wilsonpro.com/",
-		"https://staging-wilsonpro-canada-api.herokuapp.com",
-		"https://wilsonpro-canada-staging.herokuapp.com/",
-	})
-
-	port := os.Getenv("PORT")
-	if port == "" {
-		log.Fatal("PORT was not defined.")
-	} else {
-		fmt.Println("Listening on port: ", port)
-		log.Fatal(http.ListenAndServe(":"+port, handlers.CORS(methods, origins, headers)(r)))
-	}
 }
