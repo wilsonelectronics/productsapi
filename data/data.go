18268cf251416071b86d09f8ec039c7cc3bb6acc
Kevin Bateman <kbateman@wilsonelectronics.com>
2019-05-03 19:06:53 -0600

    Added back go-mssqlbd

--- data/data.go
+++ data/data.go
@@ -3,6 +3,9 @@ package data
 import (
 	"database/sql"
 	"os"
+
+	// Justification: No main.go file, and this is the only file that 'technically' uses this package
+	_ "github.com/denisenkom/go-mssqldb"
 )
 
 // GetDB makes connection to database
