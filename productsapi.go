6ac23f9f43226099d737f2c98a9deec4d353d8d2
Kevin Bateman <kbateman@wilsonelectronics.com>
2019-05-03 18:31:44 -0600

    Updates to productsapi init functionality

--- productsapi.go
+++ productsapi.go
@@ -1,6 +1,8 @@
 package productsapi
 
 import (
+	"fmt"
+	"log"
 	"net/http"
 	"productsapi/controller"
 
@@ -8,32 +10,67 @@ import (
 	"github.com/gorilla/mux"
 )
 
-// HandleFuncModel . . .
-type HandleFuncModel struct {
-	path    string
-	handler func(http.ResponseWriter, *http.Request)
+type route struct {
+	Path    string
+	Handler func(http.ResponseWriter, *http.Request)
 }
 
-// CORSOptions . . .
-func CORSOptions(moreOptions ...handlers.CORSOption) []handlers.CORSOption {
-	return append([]handlers.CORSOption{
-		handlers.AllowedMethods([]string{"GET"}),
-		handlers.AllowedHeaders([]string{"Content-Type", "*"}),
-		handlers.AllowedOrigins([]string{"http://localhost:3000"})},
-		moreOptions...)
+// HandlerOptions . . .
+type HandlerOptions struct {
+	routes      []route
+	corsOptions []handlers.CORSOption
 }
 
-// Router . . .
-func Router(moreRoutes ...HandleFuncModel) *mux.Router {
+func createRouter(routes ...route) *mux.Router {
 	r := mux.NewRouter()
-	for _, v := range append([]HandleFuncModel{
-		HandleFuncModel{path: "/tags", handler: controller.GetTags},
-		HandleFuncModel{path: "/tag/products/{tagId}", handler: controller.GetTagProducts},
-		HandleFuncModel{path: "/categories", handler: controller.GetCategories},
-		HandleFuncModel{path: "/category/products/{categoryGuid}", handler: controller.GetCategoryProducts},
-		HandleFuncModel{path: "/product/{handle}", handler: controller.GetProduct}},
-		moreRoutes...) {
-		r.HandleFunc(v.path, v.handler)
+	for _, v := range routes {
+		r.HandleFunc(v.Path, v.Handler)
 	}
 	return r
+}
+
+// Handler . . .
+func Handler(opts ...*HandlerOptions) http.Handler {
+	if opts == nil {
+		return handlers.CORS()(createRouter())
+	} else if len(opts) != 1 {
+		panic(fmt.Sprintf("Invalid number of HandlerOptions provided: %d", len(opts)))
+	}
+	return handlers.CORS(opts[0].corsOptions...)(createRouter(opts[0].routes...))
+}
+
+// AddRoute . . .
+func AddRoute(opts *HandlerOptions, path string, f func(http.ResponseWriter, *http.Request)) {
+	opts.routes = append(opts.routes, route{Path: path, Handler: f})
+}
+
+// AddCORSOption . . .
+func AddCORSOption(opts *HandlerOptions, corsKey string, corsValues ...string) {
+	var f func([]string) handlers.CORSOption
+	switch corsKey {
+	case "METHODS":
+		f = handlers.AllowedMethods
+	case "HEADERS":
+		f = handlers.AllowedHeaders
+	case "ORIGINS":
+		f = handlers.AllowedOrigins
+	default:
+		log.Fatal("Invalid CORS option type: " + corsKey + ". Must be METHODS, HEADERS, or ORIGINS")
+	}
+	opts.corsOptions = append(opts.corsOptions, f(corsValues))
+}
+
+// BaseHandlerOptions . . .
+func BaseHandlerOptions() *HandlerOptions {
+	opts := &HandlerOptions{}
+	AddRoute(opts, "/tags", controller.GetTags)
+	AddRoute(opts, "/tag/products/{tagId}", controller.GetTagProducts)
+	AddRoute(opts, "/categories", controller.GetCategories)
+	AddRoute(opts, "/category/products/{categoryGuid}", controller.GetCategoryProducts)
+	AddRoute(opts, "/product/{handle}", controller.GetProduct)
+
+	AddCORSOption(opts, "METHODS", "GET")
+	AddCORSOption(opts, "HEADERS", "Content-Type", "*")
+	AddCORSOption(opts, "ORIGINS", "http://localhost:3000")
+	return opts
 }
