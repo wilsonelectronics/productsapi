2b49398ce39fd6eea0aea8e60c0cdacb138095d4
Kevin Bateman <kbateman@wilsonelectronics.com>
2019-05-21 09:23:09 -0600

    Changed category.HeaderText to a nullable string

--- category/category.go
+++ category/category.go
@@ -16,7 +16,7 @@ type Category struct {
 	GUID        string        `json:"guid"`
 	Name        string        `json:"name"`
 	Handle      string        `json:"handle"`
-	HeaderText  string        `json:"headerText"`
+	HeaderText  ntypes.String `json:"headerText"`
 	Description string        `json:"description"`
 	ImageURL    ntypes.String `json:"imageURL"`
 }
